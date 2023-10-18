19625cbba7ddc5a3df673b092b4aba009a39d99e
Nirmalya Nayak <n.nayak263@gmail.com>
2023-10-18 14:42:48 +0530

    defined and reading port form .env

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Println("Port:", port)
 }
