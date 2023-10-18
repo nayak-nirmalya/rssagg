3d4bbb5fb240a227def27303d8c24b570ffd4f0b
Nirmalya Nayak <n.nayak263@gmail.com>
2023-10-18 17:43:04 +0530

    added handler_user to create new user

--- main.go
+++ main.go
@@ -54,6 +54,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadliness)
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -63,7 +64,7 @@ func main() {
 	}
 
 	log.Printf("Server starting on port %v", port)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal((err))
 	}
